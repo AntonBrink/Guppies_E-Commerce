68a2ccc395fccd4a33ec5971849e7feca4d0933f
Anton Brink <twiggymocha@gmail.com>
2021-07-09 00:09:50 +0200

    navbar layout done

--- src/components/mynavbar.js
+++ src/components/mynavbar.js
@@ -1,34 +1,54 @@
 import React from "react"
 import { Link } from "gatsby"
 import { FaSearch } from "@react-icons/all-files/fa/FaSearch"
+import * as navStyles from "../styles/navStyles.module.scss"
 
 const Mynavbar = () => {
   return (
-    <header>
-      <img src="" alt="" />
+    <header className={navStyles.navBar}>
+      <img src="" alt="" width="300" height="200" />
 
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/shop">Shop</Link>
-          <Link to="/contactus">Contact Us</Link>
-          <Link to="/faq">FAQ</Link>
+      <div className={navStyles.topNav}>
+        <nav className={navStyles.links}>
+          <Link className={navStyles.link} to="/">
+            Home
+          </Link>
+          <Link className={navStyles.link} to="/shop">
+            Shop
+          </Link>
+          <Link className={navStyles.link} to="/contactus">
+            Contact Us
+          </Link>
+          <Link className={navStyles.link} to="/faq">
+            FAQ
+          </Link>
         </nav>
-
-        <div className="contactBlock">
-          <a href="mailto:twiggymocha@gmail.com">twiggymocha@gmail.com</a>
-          <a href="tel:+0767177054">0767177054</a>
-        </div>
+        <input className={navStyles.searchBar} type="text" />
       </div>
 
-      <div>
-        <input type="text" />
+      <div className={navStyles.bottomNav}>
+        <div className={navStyles.contactMethods}>
+          <a className={navStyles.phone} href="mailto:twiggymocha@gmail.com">
+            twiggymocha@gmail.com
+          </a>
+          <a className={navStyles.phone} href="tel:+0767177054">
+            0767177054
+          </a>
+        </div>
+
         <div>
-          <select name="Categories" id="Categories">
-            <option value="All"></option>
-            <option value="Fish"></option>
-            <option value="Plants"></option>
-            <option value="Food"></option>
+          <label htmlFor="Categories"></label>
+          <select
+            className={navStyles.catDropDown}
+            name="Categories"
+            id="Categories"
+          >
+            <option selected value="All">
+              All
+            </option>
+            <option value="Fish">Fish</option>
+            <option value="Plants">Plants</option>
+            <option value="Food">Food</option>
           </select>
           <FaSearch></FaSearch>
         </div>
---
+++ src/pages/shop.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Shop = () => {
+  return <Layout>Shop!</Layout>
+}
+
+export default Shop
