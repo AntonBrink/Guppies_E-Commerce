a19b93ba097880fa2831da7a9b317a2c4d27d86e
Anton Brink <twiggymocha@gmail.com>
2021-07-15 02:56:50 +0200

    did homepage styling such as laying out grid and selecting banner position

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
+import { Link } from "gatsby"
+import * as homeStyles from "../styles/home.module.scss"
 
 const Homepage = () => {
-  return <Layout>Hello world!</Layout>
+  return (
+    <Layout>
+      <img src="" alt="" width="1920" height="400" />
+      <div>
+        <h1 className={homeStyles.categoryHeader}>Categories</h1>
+
+        <div className={homeStyles.categoryPanels}>
+          <Link className={homeStyles.categoryPanel}>All</Link>
+          <Link className={homeStyles.categoryPanel}>Fish</Link>
+          <Link className={homeStyles.categoryPanel}>Plants</Link>
+          <Link className={homeStyles.categoryPanel}>Food</Link>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Homepage
