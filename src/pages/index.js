ba653162279736cad994cfd32e633879ae603a2a
Anton Brink <twiggymocha@gmail.com>
2021-10-09 18:53:11 +0200

    did homepage for desktop and large laptop display

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,124 @@ import React from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
 import * as homeStyles from "../styles/home.module.scss"
+import { BiRadioCircle } from "@react-icons/all-files/bi/BiRadioCircle"
 
 const Homepage = () => {
   return (
     <Layout>
-      <img src="" alt="" width="1920" height="400" />
+      {/* <img src="" alt="" width="1920" height="400" /> */}
       <div>
-        <h1 className={homeStyles.categoryHeader}>Categories</h1>
+        {/* <h1 className={homeStyles.categoryHeader}>Categories</h1> */}
 
         <div className={homeStyles.categoryPanels}>
-          <Link className={homeStyles.categoryPanel}>All</Link>
-          <Link className={homeStyles.categoryPanel}>Fish</Link>
-          <Link className={homeStyles.categoryPanel}>Plants</Link>
-          <Link className={homeStyles.categoryPanel}>Food</Link>
+          <div className={homeStyles.fish}>
+            <svg className={homeStyles.circleSvg}>
+              <Link to="/shop" className={homeStyles.fishCatBtn}>
+                <circle
+                  className={homeStyles.backgroundCircle}
+                  cx="50%"
+                  cy="50%"
+                  r="40"
+                  stroke="white"
+                  stroke-width="3"
+                  fill="transparent"
+                />
+                <text x="50%" y="50%">
+                  Fish
+                </text>
+                <circle
+                  className={homeStyles.glowingCircle}
+                  cx="50%"
+                  cy="50%"
+                  r="10"
+                  stroke="white"
+                  stroke-width="3"
+                  fill="transparent"
+                />
+              </Link>
+            </svg>
+          </div>
+          <div className={homeStyles.tank}>
+            <svg className={homeStyles.circleSvg}>
+              <Link to="/shop" className={homeStyles.fishCatBtn}>
+                <circle
+                  className={homeStyles.backgroundCircle}
+                  cx="50%"
+                  cy="50%"
+                  r="40"
+                  stroke="white"
+                  stroke-width="3"
+                  fill="transparent"
+                />
+                <text x="50%" y="50%">
+                  Tanks
+                </text>
+                <circle
+                  className={homeStyles.glowingCircle}
+                  cx="50%"
+                  cy="50%"
+                  r="10"
+                  stroke="white"
+                  stroke-width="3"
+                  fill="transparent"
+                />
+              </Link>
+            </svg>
+          </div>
+          <div className={homeStyles.heater}>
+            <svg className={homeStyles.circleSvg}>
+              <Link to="/shop" className={homeStyles.fishCatBtn}>
+                <circle
+                  className={homeStyles.backgroundCircle}
+                  cx="50%"
+                  cy="50%"
+                  r="40"
+                  stroke="white"
+                  stroke-width="3"
+                  fill="transparent"
+                />
+                <text x="50%" y="50%">
+                  Heaters
+                </text>
+                <circle
+                  className={homeStyles.glowingCircle}
+                  cx="50%"
+                  cy="50%"
+                  r="10"
+                  stroke="white"
+                  stroke-width="3"
+                  fill="transparent"
+                />
+              </Link>
+            </svg>
+          </div>
+          <div className={homeStyles.plants}>
+            <svg className={homeStyles.circleSvg}>
+              <Link to="/shop" className={homeStyles.fishCatBtn}>
+                <circle
+                  className={homeStyles.backgroundCircle}
+                  cx="50%"
+                  cy="50%"
+                  r="40"
+                  stroke="white"
+                  stroke-width="3"
+                  fill="transparent"
+                />
+                <text x="50%" y="50%">
+                  Plants+
+                </text>
+                <circle
+                  className={homeStyles.glowingCircle}
+                  cx="50%"
+                  cy="50%"
+                  r="10"
+                  stroke="white"
+                  stroke-width="3"
+                  fill="transparent"
+                />
+              </Link>
+            </svg>
+          </div>
         </div>
       </div>
     </Layout>
